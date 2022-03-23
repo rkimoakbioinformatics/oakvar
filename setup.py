aa403599ad985d875cceba9999b9835a7d73a4c2
Ryangguk Kim <rkim@oakbioinformatics.com>
2022-03-23 14:27:51 +0900

    description update

--- setup.py
+++ setup.py
@@ -57,8 +57,8 @@ for root, dirs, files in os.walk(os.path.join('cravat', 'websubmit')):
 setup(
     name='oak-cravat',
     packages=['cravat'],
-    version='2.3.7',
-    description='OakCRAVAT, a fork of OpenCRAVAT',
+    version='2.3.8',
+    description='OakCRAVAT, further development of OpenCRAVAT by its lead architect',
     long_description=readme(),
     author='Ryangguk Kim',
     author_email='rkim@oakbioinformatics.com',
