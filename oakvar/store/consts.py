b2b46cd56eb9c5552d38094ae0a7b8f8efe6c5bb
Ryangguk Kim <rkim@oakbioinformatics.com>
2022-09-19 09:57:57 +0900

    changed default logo size

--- oakvar/store/consts.py
+++ oakvar/store/consts.py
@@ -19,7 +19,7 @@ versions_table_cols = [
     "data_size",
     "publish_time",
 ]
-logo_size = (180, 85)
+logo_size = (275, 170)
 ov_store_split_file_size = 1_000_000_000 # 1 GB parts
 #ov_store_split_file_size = 100_000_000 # 100 MB parts
 pack_ignore_fnames = [
