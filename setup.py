7dbabd3b80acd4e49b6271cf07edc5a0f5213f4a
Ryangguk Kim <rkim@oakbioinformatics.com>
2023-02-18 18:29:04 +0800

    secondary input change

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ walk_and_add("oakvar", oakvar_files)
 walk_and_add("cravat", cravat_files)
 setup(
     name="oakvar",
-    version="2.7.40",
+    version="2.7.41",
     description="A genomic variant analysis platform",
     long_description=long_description,
     long_description_content_type="text/x-rst",
