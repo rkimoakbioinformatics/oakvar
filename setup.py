2b6273bf47455a2584e129342401b73916ae95da
Ryangguk Kim <rkim@oakbioinformatics.com>
2023-03-30 11:58:00 +0800

    v2.9.0 to be released

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ walk_and_add("oakvar", oakvar_files)
 walk_and_add("cravat", cravat_files)
 setup(
     name="oakvar",
-    version="2.8.40",
+    version="2.9.0"
     description="A genomic variant analysis platform",
     long_description=long_description,
     long_description_content_type="text/x-rst",
