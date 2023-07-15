eec68640936af592f8e807dc0ee7ef0c7ca537a3
Ryangguk Kim <rkim@oakbioinformatics.com>
2023-07-15 19:06:26 +0800

    Fixed ov store delete.

--- oakvar/cli/store/__init__.py
+++ oakvar/cli/store/__init__.py
@@ -188,7 +188,7 @@ def add_parser_fn_store_delete(subparsers):
         "module_name", help="Name of the module to delete"
     )
     parser_cli_store_delete.add_argument(
-        "--version", default=None, help="Version of the module to delete"
+        "--version", dest="code_version", default=None, help="Version of the module to delete"
     )
     parser_cli_store_delete.add_argument(
         "--all",
