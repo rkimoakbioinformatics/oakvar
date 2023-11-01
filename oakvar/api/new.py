7cee93d0c8f12425481e48c59c870c6e35f3005b
Ryangguk Kim <rkim@oakbioinformatics.com>
2023-10-31 21:53:26 -0400

    Updated license.

--- oakvar/api/new.py
+++ oakvar/api/new.py
@@ -2,16 +2,16 @@
 # 
 # Copyright (c) 2023 Oak Bioinformatics, LLC
 # 
-# This program is dual licensed under the Affero GPL-3.0 or later for non-commercial, 
-# open source use, and a commercial license, which is available for purchase, 
-# for commercial use.
+# This program is dual licensed under the Affero GPL-3.0 or later for 
+# non-commercial and open source use, and under a commercial license, 
+# which is available for purchase, for closed-source or commercial use.
 # 
-# For commercial use, please contact Oak Bioinformatics, LLC for obtaining a
-# commercial license. OakVar commercial license does not impose the Affero GPL
-# open-source licensing terms, conditions, and limitations. To obtain a
-# commercial-use license of OakVar, please visit our website at
-# https://oakbioinformatics.com or contact us at info@oakbioinformatics.com for
-# more information.
+# For the commercial use, please contact Oak Bioinformatics, LLC 
+# for obtaining such a license. OakVar commercial license does not impose 
+# the Affero GPL open-source licensing terms, conditions, and limitations. 
+# To obtain a commercial-use license of OakVar, please visit our website at
+# https://oakbioinformatics.com or contact us at info@oakbioinformatics.com 
+# for more information.
 # 
 # ================
 # OpenCRAVAT
