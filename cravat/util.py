06d029273d0e97ca92b4d1c1808966fb02a66a96
Ryangguk Kim <rkim@oakbioinformatics.com>
2026-02-10 22:47:37 +0900

    compat

--- cravat/util.py
+++ cravat/util.py
@@ -1,1 +1,27 @@
 from oakvar.lib.util import *
+
+aa_321 = {
+    "Asp": "D",
+    "Ser": "S",
+    "Gln": "Q",
+    "Lys": "K",
+    "Trp": "W",
+    "Asn": "N",
+    "Pro": "P",
+    "Thr": "T",
+    "Phe": "F",
+    "Ala": "A",
+    "Gly": "G",
+    "Cys": "C",
+    "Ile": "I",
+    "Leu": "L",
+    "His": "H",
+    "Arg": "R",
+    "Met": "M",
+    "Val": "V",
+    "Glu": "E",
+    "Tyr": "Y",
+    "Ter": "*",
+    "": "",
+}
+
